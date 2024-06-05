a1fb2cfb935f22ce11588110a6d069fd6fe0b151
Fabricio Araujo <faaraujo@student.42lisboa.com>
2024-06-05 16:31:01 +0100

    (update) clean code

--- dcs/code.md
+++ dcs/code.md
@@ -77,6 +77,35 @@ int	main(int argc, char *argv[])
 
 ## Exercício 01
 ```cpp
+/**
+ * @brief Na inicialização do programa, a agenda está vazia e o usuário é
+ * solicitado a inserir um dos três comandos. O programa aceita apenas ADD,
+ * SEARCH e EXIT
+ * @param ADD Metodo para salvar um novo contacto
+ * 		@brief Se o usuário inserir este comando, ele será solicitado a inserir
+ * 		as informações do novo contato, um campo por vez. Depois que todos
+ * 		os campos forem preenchidos, adicione o contato à lista telefônica.
+ *		@brief Os campos de contato são: nome, sobrenome, apelido, número
+ *		de telefone e segredo mais obscuro. 
+ *		Um contato salvo não pode ter campos vazios
+ * @param SEARCH display a specific contact
+ *		@brief Exibe os contatos salvos como uma lista de 4 colunas:
+ *		índice, nome, sobrenome e apelido.
+ *		@brief Cada coluna deve ter 10 caracteres de largura. 
+ *		Um caractere de barra vertical ('|') os separa. 
+ *		O texto deve estar alinhado à direita. Se o texto for maior que
+ *		a coluna, ele deverá ser truncado e o último caractere exibível
+ *		deverá ser substituído por um ponto (’.’).
+ *		@brief Em seguida, solicite novamente ao usuário o índice da entrada
+ *		a ser exibido. Se o índice estiver fora do intervalo ou errado, defina
+ *		um comportamento relevante. Caso contrário, exiba as informações de 
+ *		contato, um campo por linha.
+ * @param EXIT The program quits and the contacts are lost forever!
+ *		@brief Qualquer outra entrada é descartada.
+ *		@brief Depois que um comando for executado corretamente, o programa 
+ *		aguarda outro. Ele para quando o usuário digita EXIT.
+ *		@brief Dê um nome relevante ao seu executável.
+*/
 int	main(void)
 {
 	PhoneBook phonebook;
--- src/ex01/PhoneBook.cpp
+++ src/ex01/PhoneBook.cpp
@@ -6,41 +6,12 @@
 /*   By: faaraujo <faaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 20:40:41 by faaraujo          #+#    #+#             */
-/*   Updated: 2024/05/31 18:53:38 by faaraujo         ###   ########.fr       */
+/*   Updated: 2024/06/05 16:15:50 by faaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-/**
- * @brief Na inicialização do programa, a agenda está vazia e o usuário é
- * solicitado a inserir um dos três comandos. O programa aceita apenas ADD,
- * SEARCH e EXIT
- * @param ADD Metodo para salvar um novo contacto
- * 		@brief Se o usuário inserir este comando, ele será solicitado a inserir
- * 		as informações do novo contato, um campo por vez. Depois que todos
- * 		os campos forem preenchidos, adicione o contato à lista telefônica.
- *		@brief Os campos de contato são: nome, sobrenome, apelido, número
- *		de telefone e segredo mais obscuro. 
- *		Um contato salvo não pode ter campos vazios
- * @param SEARCH display a specific contact
- *		@brief Exibe os contatos salvos como uma lista de 4 colunas:
- *		índice, nome, sobrenome e apelido.
- *		@brief Cada coluna deve ter 10 caracteres de largura. 
- *		Um caractere de barra vertical ('|') os separa. 
- *		O texto deve estar alinhado à direita. Se o texto for maior que
- *		a coluna, ele deverá ser truncado e o último caractere exibível
- *		deverá ser substituído por um ponto (’.’).
- *		@brief Em seguida, solicite novamente ao usuário o índice da entrada
- *		a ser exibido. Se o índice estiver fora do intervalo ou errado, defina
- *		um comportamento relevante. Caso contrário, exiba as informações de 
- *		contato, um campo por linha.
- * @param EXIT The program quits and the contacts are lost forever!
- *		@brief Qualquer outra entrada é descartada.
- *		@brief Depois que um comando for executado corretamente, o programa 
- *		aguarda outro. Ele para quando o usuário digita EXIT.
- *		@brief Dê um nome relevante ao seu executável.
-*/
 PhoneBook::PhoneBook() : contactIndex(0) {}
 PhoneBook::~PhoneBook() { std::cout << "PhoneBook deteled!" << std::endl; }
 std::string	strlenCheck(std::string str);
